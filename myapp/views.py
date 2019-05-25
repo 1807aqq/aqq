2d66aa3fc9542886038e095e8169557311782402
DragonFighterGod <898935866@qq.com>
2019-05-25 11:01:15 +0800

    finsh loan and products busines

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,4 @@
+import random
 
 from django.db.models import Q, Sum, Count
 from django.contrib import messages
@@ -6,7 +7,7 @@ from django.urls import reverse
 
 from tonghuashun.news import *
 
-from myapp.models import User, Industry ,Product ,Institutions
+from myapp.models import User, Industry, Product, Institutions, Loan
 
 from tools.msg_send import get_code, confirm
 from tools.Is_login import is_login
@@ -17,6 +18,9 @@ from django.shortcuts import render, redirect
 from myapp.models import User
 
 # 注册页面
+from tools.save_info import mortgage_save, get_info
+
+
 def register(request):
     if request.method == "GET":
         # msg = messages.get_messages(request)
@@ -31,7 +35,7 @@ def register(request):
             messages.add_message(request, messages.INFO, '该手机号已注册！！')
             return redirect('aqq:register')
         phone_message = request.POST.get('phone-message')
-        code = confirm(phone,phone_message)
+        code = confirm(phone, phone_message)
         print(code)
         if not code:
             messages.add_message(request, messages.INFO, '验证码错误！！')
@@ -44,11 +48,11 @@ def register(request):
         banks = request.POST.get('category')
         city = request.POST.get('city')
         open_bank = request.POST.get('open_bank')
-        print(phone,phone_message,passwd,name,identity,bank_num,banks,city,open_bank)
+        print(phone, phone_message, passwd, name, identity, bank_num, banks, city, open_bank)
         new_user = User()
         new_user.nickname = nickname
         new_user.phone = phone
-        new_user.passwd =passwd
+        new_user.passwd = passwd
         new_user.name = name
         new_user.identity = identity
         new_user.bank_num = bank_num
@@ -58,6 +62,7 @@ def register(request):
         new_user.save()
         return redirect("aqq:account")
 
+
 # 登陆页面
 def login(request):
     if request.method == "GET":
@@ -66,34 +71,34 @@ def login(request):
     elif request.method == "POST":
         phone = request.POST.get('name')
         passwd = request.POST.get('password')
-        print(phone,passwd,"@@@@@@@@@@@2")
+        print(phone, passwd, "@@@@@@@@@@@2")
         if User.objects.filter(phone=phone, passwd=passwd).exists():
             user = User.objects.filter(phone=phone, passwd=passwd)[0]
             if phone == user.phone and passwd == user.passwd:
-
+                request.session['uid'] = user.uid
                 return redirect(reverse('aqq:account'))
 
             else:
-                context = {'msg':'用户名或密码错误！！！'}
-                return render(request,'login.html',context)
+                context = {'msg': '用户名或密码错误！！！'}
+                return render(request, 'login.html', context)
 
         else:
-            messages.add_message(request,messages.INFO,'用户名不存在！！')
+            messages.add_message(request, messages.INFO, '用户名不存在！！')
             return redirect('aqq:register')
 
 
 def my_account(request):
     user = is_login(request)
-    return render(request, 'my-account.html',{'user':user})
+    return render(request, 'my-account.html', {'user': user})
 
 
 # 进入首页
 def go_index(request):
     user = is_login(request)
-    print('@@@@@@@@@@@',user)
+    print('@@@@@@@@@@@', user)
     if request.method == 'GET':
         now_time = time.localtime().tm_min
-        if now_time%10 == 0:
+        if now_time % 10 == 0:
             Downlode()
         datas = Industry.objects.all()
         sum_amount = Product.objects.all().aggregate(Sum('amount'))
@@ -107,25 +112,23 @@ def go_index(request):
             db['news_time'] = data.time
             msgs[data.ip] = db
         return render(request, 'index.html',
-                      {'datas':msgs,
-                       'new_pros':new_pro[:3],
-                       'pros':new_pro[5:8],
-                      'sum_amount':sum_amount,
-                       'count':count,
-                       'user':user})
-
-
-
+                      {'datas': msgs,
+                       'new_pros': new_pro[:3],
+                       'pros': new_pro[5:8],
+                       'sum_amount': sum_amount,
+                       'count': count,
+                       'user': user})
 
 
 # 进入新闻详情页
-def go_details(request,id):
+def go_details(request, id):
     user = is_login(request)
     msg = Industry.objects.filter(ip=id)[0]
     title = msg.name
     time = msg.time
     info = base64.b16decode((msg.info).encode()).decode()
-    return render(request, 'press-details.html',locals())
+    return render(request, 'press-details.html', locals())
+
 
 # 新闻中心
 def press_center(request):
@@ -136,107 +139,112 @@ def press_center(request):
         db = {}
         msg = base64.b16decode(data.info.encode()).decode()
         db['ip'] = data.ip
-        db['info'] = ','.join(re.findall(r'[\u4e00-\u9fa5]{10}',msg))[:50]
+        db['info'] = ','.join(re.findall(r'[\u4e00-\u9fa5]{10}', msg))[:50]
         db['title'] = data.name
         msgs[data.ip] = db
-    return render(request, 'press-center.html', {'datas': msgs,'user':user})
+    return render(request, 'press-center.html', {'datas': msgs, 'user': user})
+
 
 # 投资理财页面
-def invest(request,tid,sid,did,page):
+def invest(request, tid, sid, did, page):
     user = is_login(request)
     if request.method == 'GET':
-        type_list = ['type=1','type=2','type=3','type=4','type=5']
+        type_list = ['type=1', 'type=2', 'type=3', 'type=4', 'type=5']
         rate_list = ['y_rate__lt=11', 'y_rate__gt=11,y_rate__lt=13', 'y_rate__gt=13']
         time_list = ['time_limit=24', 'time_limit=36', 'time_limit=48']
         select = {
-            'tid': [tid,type_list],
-            'sid': [sid,rate_list],
-            'did': [did,time_list]
+            'tid': [tid, type_list],
+            'sid': [sid, rate_list],
+            'did': [did, time_list]
         }
         filters = []
         for k in select:
             if select[k][0] == '0':
                 continue
             else:
-                filters.append(select[k][1][int(select[k][0])-1])
+                filters.append(select[k][1][int(select[k][0]) - 1])
         data = {}
         newpage = int(page)
         m = newpage * 8
         if len(filters) > 0:
             for i in filters:
                 if ',' in i:
-                    x,y = i.split(',')
-                    a,b = x.split('=')
-                    data[a]=b
-                    a,b = y.split('=')
-                    data[a]=b
+                    x, y = i.split(',')
+                    a, b = x.split('=')
+                    data[a] = b
+                    a, b = y.split('=')
+                    data[a] = b
                 else:
-                    a,b = i.split("=")
-                    data[a]=b
+                    a, b = i.split("=")
+                    data[a] = b
             print(data)
-            produces = Product.objects.filter(**data)[m-8:m]
+            produces = Product.objects.filter(**data)[m - 8:m]
 
         else:
-            produces = Product.objects.all()[m-8:m]
-        newpage +=1
+            produces = Product.objects.all()[m - 8:m]
+        newpage += 1
         data = {
             'products': produces,
             'tid': tid,
             'sid': sid,
             'did': did,
-            'page':newpage,
-            'user':user
+            'page': newpage,
+            'user': user
         }
-        return render(request,'invest.html',data)
-
+        return render(request, 'invest.html', data)
 
 
 # 产品详情页
-def details(request,pid):
+def details(request, pid):
     user = is_login(request)
 
     pro = Product.objects.get(id=int(pid))
-    pay = ['先息后本','先本后息','等额本金','等额本息']
-    payment = pay[pro.payment-1]
+    pay = ['先息后本', '先本后息', '等额本金', '等额本息']
+    payment = pay[pro.payment - 1]
     data = pro.info.split('&')
     print(payment)
     data1 = data[0]
     data2 = data[1]
     data3 = data[2]
     return render(request, "details.html",
-                  {'pro':pro,
-                   'payment':payment,
-                   'data1':data1,
-                   'data2':data2,
-                   'data3':data3,
-                   'user':user})
+                  {'pro': pro,
+                   'payment': payment,
+                   'data1': data1,
+                   'data2': data2,
+                   'data3': data3,
+                   'user': user})
+
+
 # 安全保障
 def secure(request):
     user = is_login(request)
-    return render(request,'secure.html',{'user':user})
+    return render(request, 'secure.html', {'user': user})
+
 
 # 关于我们
 def anenst(request):
     user = is_login(request)
-    return render(request,'anenst.html',{'user':user})
-
+    return render(request, 'anenst.html', {'user': user})
 
 
 # 借贷专区
 
 def borrow_money(request):
     user = is_login(request)
-    return render(request,'borrow-money.html',{'user':user})
+    return render(request, 'borrow-money.html', {'user': user})
+
 
 # 新手指南
-def  guide(request):
+def guide(request):
     user = is_login(request)
-    return  render(request,'Beginners-Guide.html',{'user':user})
+    return render(request, 'Beginners-Guide.html', {'user': user})
 
-#帮助中心
+
+# 帮助中心
 def help(request):
     user = is_login(request)
-    return  render(request,'help-center.html',{'user':user})
+    return render(request, 'help-center.html', {'user': user})
+
 
 # 充值
 def recharge(request):
@@ -244,19 +252,20 @@ def recharge(request):
 
     money = request.POST.get('money')
 
-    user.em_contact=money
+    user.em_contact = money
     User.objects.filter('em_contact')
-    
-    return render(request,'my-recharge.html',{'user':user})
 
-#提现
+    return render(request, 'my-recharge.html', {'user': user})
+
+
+# 提现
 def withdraw(request):
     user = is_login(request)
-    return render(request,'my-withdraw.html',{'user':user})
+    return render(request, 'my-withdraw.html', {'user': user})
 
 
 # 发送验证码函数
-def codes(request,phone):
+def codes(request, phone):
     user = is_login(request)
     get_code(phone)
     return JsonResponse({
@@ -264,69 +273,101 @@ def codes(request,phone):
     })
 
 
-
 # 申请质押贷款
 def pledge(request):
     user = is_login(request)
     if request.method == 'GET':
-        return render(request,'Pledge-loan.html',{'user':user})
+        return render(request, 'Pledge-loan.html', {'user': user})
     elif request.method == 'POST':
         if request.user:
-            pass
+            params = get_info(request)
+            type = 1
+            #  插入借贷表
+            loan = Loan()
+            # 同时生成一个产品表
+            product = Product()
+            mortgage_save(loan, product, user, *params, type)
+
         else:
-            data={
-                'msg':'您还未登录！！',
-                'user': user
-            }
-    return render(request,'Pledge-loan.html',data)
+            messages.add_message(request, messages.INFO, '请先登录！！')
+            return redirect(reverse('aqq:login'))
+        return redirect(reverse('aqq:buy_success'))
+
 
 # 申请抵押贷
 def mortgage(request):
     user = is_login(request)
     if request.method == 'GET':
-        return render(request,'mortgage-loan.html',{'user':user})
+        return render(request, 'mortgage-loan.html', {'user': user})
     elif request.method == 'POST':
         if request.user:
-            pass
+            params = get_info(request)
+            type = 2
+            #  插入借贷表
+            loan = Loan()
+            # 同时生成一个产品表
+            product = Product()
+            mortgage_save(loan, product, user, *params, type)
         else:
-            data={
-                'msg':'您还未登录！！',
+            data = {
+                'msg': '您还未登录！！',
                 'user': user
             }
-    return render(request,'mortgage-loan.html',data)
+            messages.add_message(request, messages.INFO, '请先登录！！')
+            return redirect(reverse('aqq:login'))
+    return redirect(reverse('aqq:buy_success'))
 
 
 # 申请创业贷
 def venture(request):
     user = is_login(request)
     if request.method == 'GET':
-        return render(request,'Venture-loan.html',{'user':user})
+        return render(request, 'Venture-loan.html', {'user': user})
     elif request.method == 'POST':
         if request.user:
-            pass
+            params = get_info(request)
+            type = 3
+            #  插入借贷表
+            loan = Loan()
+            # 同时生成一个产品表
+            product = Product()
+            mortgage_save(loan, product, user, *params, type)
         else:
-            data={
-                'msg':'您还未登录！！',
+            data = {
+                'msg': '您还未登录！！',
                 'user': user
             }
-    return render(request,'Venture-loan.html',data)
+            messages.add_message(request, messages.INFO, '请先登录！！')
+            return redirect(reverse('aqq:login'))
+    return redirect(reverse('aqq:buy_success'))
+
 
 # 申请保单贷
 def policy(request):
     user = is_login(request)
     if request.method == 'GET':
-        return render(request,'Policy-loan.html',{'user':user})
+        return render(request, 'Policy-loan.html', {'user': user})
     elif request.method == 'POST':
         if request.user:
-            pass
+            params = get_info(request)
+            type = 4
+            #  插入借贷表
+            loan = Loan()
+            # 同时生成一个产品表
+            product = Product()
+            mortgage_save(loan, product, user, *params, type)
         else:
-            data={
-                'msg':'您还未登录！！',
-                'user': user
-            }
-    return render(request,'Policy-loan.html',data)
+
+            messages.add_message(request, messages.INFO, '请先登录！！')
+            return redirect(reverse('aqq:login'))
+    return redirect(reverse('aqq:buy_success'))
+
 
 def logout(request):
     request.session.flush()
     return redirect(reverse('aqq:home'))
 
+
+# 购买成功页面
+def buy_success(requst):
+    return render(requst, 'buy-success.html')
---
+++ tools/save_info.py
@@ -0,0 +1,53 @@
+import random
+
+
+# 抵押贷款保存信息
+
+def get_info(request):
+    amount = request.POST.get('amount')
+    title = request.POST.get('title')
+    customertype = request.POST.get('customertype')
+    livetime = request.POST.get('livetime')
+    period = request.POST.get('period')
+    lenders = request.POST.get('lenders')
+    lendmethod = request.POST.get('lendmethod')
+    livespace = request.POST.get('livespace')
+    workertime = request.POST.get('workertime')
+    salary = request.POST.get('salary')
+    info = request.POST.get('info')
+    use = request.POST.get('use')
+    files = request.FILES.get('files')
+    return (amount,title,customertype,livetime,period,lenders,lendmethod,livespace,workertime,salary,info,use)
+
+def mortgage_save(loan,product,user,amount, title, customertype, livetime, period, lenders, lendmethod, livespace,
+                  workertime, salary,info, use,type):
+    # 保存借贷信息表
+    # 插入借贷表
+    loan.uid = user.uid
+    loan.amount = amount
+    loan.title = title
+    loan.cus_type = customertype
+    loan.time_period = livetime
+    loan.time_limit = period
+    loan.instotution = lenders
+    loan.payment = lendmethod
+    loan.city = livespace
+    loan.work_time = workertime
+    loan.salary_after_tax = salary
+    loan.purpose = use
+    loan.info = info
+    loan.save()
+    # 同时生成一个产品表
+    # 一个产品的id为一个借贷的id
+    product.id = loan.id
+    product.name = title
+    product.type = type
+    dic = {24: 10.3, 36: 12.7, 48: 14}
+    product.y_rate = dic[int(period)]
+    product.time_limit = period
+    product.amount = int(amount) / 10000
+    product.risk = random.choice([3.5, 4.5, 4, 3])
+    product.payment = lendmethod
+    product.institution_id = lenders
+    product.info = info
+    product.save()
